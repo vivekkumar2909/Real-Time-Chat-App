43094fe588115dd0d9a24279ec8b5056aa9de0ec
vivekkumar2909 <rajvivek620065@gmail.com>
2024-03-29 21:51:12 +0530

    Auth Form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,7 @@
 'use client';
-import React, { useState } from 'react'
+import Input from '@/app/components/Input';
+import React, { useCallback, useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -8,7 +10,50 @@ export default function AuthForm() {
     const [variant ,setVariant] = useState<Variant>('LOGIN');
     const [isLoading , setIsLoading] = useState(false);
 
+    const toggleVariant = useCallback(()=>{
+      if(variant === 'LOGIN'){
+        setVariant('REGISTER');
+
+      }else{
+        setVariant('LOGIN');
+      }
+    },[variant])
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<FieldValues>({
+      defaultValues :{
+        name:"",
+        email:"",
+        password:""
+      }
+    })
+
+    const onsubmit : SubmitHandler<FieldValues>=(data)=>{
+      setIsLoading(true);
+      console.log(data);
+      if(variant === 'LOGIN'){
+        //Axios NextAuth SignIn
+      }
+      if(variant === 'REGISTER')
+      {
+        //Next Resister
+      }
+    }
+
+    const socialAction = (action: string)=>{
+      setIsLoading(true);;
+    } 
+
   return (
-    <div>AuthForm</div>
+    <div className=' mt-8 sm:max-auto sm:w-full sm:max-w-md'>
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className=' space-y-6' onSubmit={handleSubmit(onsubmit)}>
+          <Input/>
+        </form>
+      </div>
+    </div>
   )
 }
---
+++ app/components/Input.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react'
+import clsx from 'clsx'
+import { FieldError,FieldValues,UseFormRegister } from 'react-hook-form'
+
+interface InputProps{
+    label : string
+    id : string
+    type? : string
+    required? : string
+    register : UseFormRegister<FieldValues>,
+    error : FieldError,
+    disabled? : boolean
+    
+
+}
+
+export default function Input() {
+  return (
+    <div>Input</div>
+  )
+}
